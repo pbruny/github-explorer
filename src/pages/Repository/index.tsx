f1d91f84cfd52f61a8d3f0c76631ca1b97862e77
Paulo Lima <paulobruny@gmail.com>
2020-07-15 14:47:31 -0300

    remove "abertas" from issues

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -77,7 +77,7 @@ const Repository: React.FC = () => {
             </li>
             <li>
               <strong>{repository.open_issues_count}</strong>
-              <span>Issues abertas</span>
+              <span>Issues</span>
             </li>
           </ul>
         </RepositoryInfo>
