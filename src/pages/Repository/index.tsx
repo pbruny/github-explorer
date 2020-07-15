a788219dac30d4e10bcc091f81993123e0b5b4c0
Paulo Lima <paulobruny@gmail.com>
2020-07-15 14:33:19 -0300

    finish repo info page, fetch and style

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,100 @@
-import React from 'react'
-import { Title } from './style'
+import React, { useState, useEffect } from 'react'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import { Header, RepositoryInfo, Issues } from './style'
+import logo from '../../assets/images/logo.svg'
+import api from '../../services/api'
+
+interface Repository {
+  full_name: string,
+  description: string,
+  stargazers_count: number,
+  forks_count: number,
+  open_issues_count: number,
+  owner: {
+    avatar_url: string,
+    login: string
+  },
+}
+
+interface Issue {
+  title: string,
+  id: number,
+  html_url: string,
+  user: {
+    login: string
+  }
+}
+
+interface RepositoryParams {
+  repository: string
+
+}
 
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null)
+  const [issues, setIssues] = useState<Issue[]>([])
+
+  const { params } = useRouteMatch<RepositoryParams>()
+
+  useEffect(() => {
+    api.get(`/repos/${params.repository}`).then(response => {
+      setRepository(response.data)
+    })
+
+    api.get(`/repos/${params.repository}/issues`).then(response => {
+      setIssues(response.data)
+    })
+  }, [params.repository])
+
   return (
-    <Title>Hallo Caralho Repository</Title>
+    <>
+      <Header >
+        <img src={logo} alt="Github Explorer Logo" />
+        <Link to={`/`} >
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
+
+      <Issues>
+        {issues.map(issue => (
+          <a href={issue.html_url} key={issue.id} rel="noopener noreferrer" target="_blank" >
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Issues>
+    </>
   )
 }
 
--- src/pages/Repository/style.ts
+++ src/pages/Repository/style.ts
@@ -4,3 +4,136 @@ export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
 `
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 20px 0;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color 0.5s;
+
+    &:hover {
+      color: #666;
+    }
+
+    svg {
+      margin-right: 4px;
+    }
+
+  }
+`
+
+export const RepositoryInfo = styled.section`
+  margin-top: 80px;
+  
+  header {
+    display: flex;
+    align-items: center;
+    
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #737380;
+        margin-top: 4px;
+      }
+    }
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+    }
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin-top: 40px;
+
+    li {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      & + li {
+        margin-left: 80px;
+      }
+
+      strong {
+        display: block;
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #6c6c80;
+        font-size: 16px;
+      }
+    }
+  }
+`
+
+export const Issues = styled.div`
+  
+  margin-top: 80px;
+
+  a{
+    display: block;
+    text-decoration: none;
+    background-color: #FFF;
+    width: 100%;
+    padding: 24px;
+    border-radius: 5px;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.5s;
+
+    &:last-child {
+      margin-bottom: 16px;
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3D3D4D;
+      }
+
+      p {
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+
+`
